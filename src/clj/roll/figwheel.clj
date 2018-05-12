5439ccde18ece82122dada7381b85f04a0491984
dimovich <dimovich@gmail.com>
2018-05-12 23:43:37 +0300

    ...

--- src/clj/roll/figwheel.clj
+++ src/clj/roll/figwheel.clj
@@ -13,14 +13,14 @@
           :source-map    true}))
 
 
-(defn figwheel [& [opts]]
+(defn figwheel [& [{:keys [extra-source-dirs] :as opts}]]
   (-> {:figwheel-options {:css-dirs         css-dirs
                           :nrepl-port       3312
                           :nrepl-middleware ["cider.nrepl/cider-middleware"
                                              "cemerick.piggieback/wrap-cljs-repl"]}
        :all-builds [{:id           "dev"
                      :figwheel     (or (select-keys opts [:on-jsload]) true)
-                     :source-paths source-dirs
+                     :source-paths (into source-dirs extra-source-dirs)
                      :compiler     dev-config}]}
 
       (update :figwheel-options merge
@@ -28,16 +28,7 @@
                             [:css-dirs :server-port
                              :nrepl-middleware :nrepl-port])))
         
-      (start-figwheel!))
-
-  
-  #_(let [opts (when-read [data "figwheel.edn"]
-                 (clojure.edn/read-string data))]
-      
-      #_(update :figwheel-options merge
-                (some-> opts (select-keys
-                              [:css-dirs :server-port
-                               :nrepl-middleware :nrepl-port])))))
+      (start-figwheel!)))
 
 
 
