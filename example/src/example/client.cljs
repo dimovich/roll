621481c15902c73681b7cdf9856655fdbb7fc546
dimovich <dimovich@gmail.com>
2019-09-20 21:06:37 +0300

    add Sente msg handlers to example project

--- example/src/example/client.cljs
+++ example/src/example/client.cljs
@@ -1,6 +1,38 @@
-(ns example.client
-  (:require [roll.sente :as sente]))
+(ns ^:figwheel-hooks example.client
+  (:require [roll.sente :as sente :refer [event-msg-handler]]
+            [reagent.core :as r]))
+
+
+
+(defmethod event-msg-handler :chsk/handshake
+  [{:as ev-msg :keys [?data]}]
+  (let [[_ _ [data]] ?data]
+    (js/console.log "got handshake" data)))
+
+
+
+(defn app []
+  (r/with-let [resp (r/atom "")]
+    [:div
+     [:button
+      {:on-click
+       #(sente/send-msg
+         [:client/click (rand-int 10)]
+         {:timeout-ms 1000
+          :cb (fn [cb-reply] (reset! resp cb-reply))})}
+      "send"]
+     [:div "Got " (str @resp)]]))
+
+
+
+(defn ^:after-load mount-app []
+  (r/render
+   [app]
+   (.getElementById js/document "app")))
+
+
 
 
 (defn ^:export init []
-  (sente/start-router!))
+  (sente/start-router!)
+  (mount-app))
--- example/src/example/server.clj
+++ example/src/example/server.clj
@@ -1,12 +1,32 @@
 (ns example.server
   (:require [roll.core :as roll]
+            [roll.sente :refer [event-msg-handler]]
             [hiccup.core :as hiccup]
+            [taoensso.timbre :refer [info]]
             [ring.util.response :as resp]
             [ring.middleware.session :as ring-session]
             [ring.middleware.anti-forgery :as anti-forgery]
             [ring.middleware.session.cookie :as cookie]))
 
 
+
+(defn index [req]
+  (resp/response
+   (hiccup/html
+    [:html
+     [:head [:meta {:charset "UTF-8"}]]
+     [:body
+      [:div {:id "app"}]
+      ;; Sente CSRF
+      [:div#sente-csrf-token
+       {:data-csrf-token (:anti-forgery-token req)}]
+
+      [:script {:src "js/main.js" :type "text/javascript"}]
+      [:script "example.client.init()"]]])))
+
+
+
+;; we need cookies for websocket session
 (defn wrap-session [handler]
   (-> (anti-forgery/wrap-anti-forgery handler)
       (ring-session/wrap-session
@@ -14,21 +34,22 @@
         :store (cookie/cookie-store {:key "example.cookie!!"})})))
 
 
-(defn index [req]
-  (resp/response
-   (hiccup/html
-    [:html
-     [:head
-      [:meta {:charset "UTF-8"}]]
-     [:body
-      [:div "hello world"]
-    
-      ;; Sente CSRF
-      [:div#sente-csrf-token
-       {:data-csrf-token (:anti-forgery-token req)}]
 
-      [:script {:src "js/main.js" :type "text/javascript"}]
-      [:script "example.client.init()"]]])))
+;; websocket message handlers. add more as needed
+
+(defmethod event-msg-handler :client/click
+  [{:as ev-msg :keys [client-id ?data ?reply-fn]}]
+  (info ?data "from" client-id)
+  (when ?reply-fn
+    (?reply-fn ["some" {:reply ?data}])))
+
+
+
+(defn handshake-data-fn
+  "Initial data sent to client."
+  [{{:keys [client-id]} :params :as req }]
+  [["some handshake data for" client-id]])
+
 
 
 (defn -main []
