632442b65b6667a13d761f0a9a4a03e2de8385e4
dimovich <dimovich@gmail.com>
2022-07-13 23:50:09 +0300

    fix build.clj; add pom.xml template

--- build.clj
+++ build.clj
@@ -11,12 +11,18 @@
   (b/delete {:path "target"}))
 
 (defn jar [_]
-  (b/write-pom {:class-dir class-dir
+  (b/write-pom {:src-pom "template/pom.xml"
+                :class-dir class-dir
                 :lib lib
                 :version version
                 :basis basis
                 :src-dirs ["src"]})
-  (b/copy-dir {:src-dirs ["src"]
+  
+  (b/copy-dir {:src-dirs ["src/clj" "src/cljc" "src/cljs"]
                :target-dir class-dir})
+  
+  (b/copy-file {:src "src/data_readers.clj"
+                :target (str class-dir "/data_readers.clj")})
+  
   (b/jar {:class-dir class-dir
           :jar-file jar-file}))
