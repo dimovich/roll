78fa10e9319cdc8e667d9c2e292e7b5f8e48a03f
dimovich <dimovich@gmail.com>
2022-07-13 15:36:33 +0300

    up deps

---
+++ build.clj
@@ -0,0 +1,22 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]))
+
+(def lib 'dimovich/roll)
+(def version (format "0.3.%s" (b/git-count-revs nil)))
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn jar [_]
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis basis
+                :src-dirs ["src"]})
+  (b/copy-dir {:src-dirs ["src"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
